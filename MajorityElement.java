751f4194a51f1a666cf2183add60851ffc9ddb61
Kesavan R <49429932+Kesavan-Raman@users.noreply.github.com>
2022-12-28 20:49:46 +0530

    Add files via upload

---
+++ MajorityElement.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class MajorityElement {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = {3, 4, 4, 5, 2, 4, 4};
+
+        int half = arr.length / 2;
+        
+        Arrays.sort(arr);
+
+
+    }
+}
